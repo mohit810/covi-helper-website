37b6def44e8478c5084a7934c36f3cd87b495c9f
Gh0sT <iammohit810@gmail.com>
2021-05-30 20:40:52 +0530

    favicons and small changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,78 +6,78 @@ function Footer() {
   const {t} = useTranslation();
 
   return (
-    <footer className="flex flex-col flex-wrap pt-2 pb-5">
-      <div className="flex-1 mb-1.5">
-        <a
-            className="text-2xl py-1 px-2 rounded-xl text-green-600 bg-green-100"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Covid-Helper
-        </a>
-      </div>
+      <footer className="flex flex-col flex-wrap pt-2 pb-5">
+        <div className="flex-1 mb-1.5">
+          <a
+              className="text-2xl py-1 px-2 rounded-xl text-green-600 bg-green-100"
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Covid-Helper
+          </a>
+        </div>
 
-      <div className="flex-1 mb-2.5">
-        <a
-            className="text-2xl py-1 px-2 "
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Let's help each other in this pandemic
-        </a>
-      </div>
+        <div className="flex-1 mb-2.5">
+          <a
+              className="text-2xl py-1 px-2 "
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Let's help each other in this pandemic
+          </a>
+        </div>
 
-      {/*<h5>{t('We stand with everyone fighting on the frontlines')}</h5>*/}
+        {/*<h5>{t('We stand with everyone fighting on the frontlines')}</h5>*/}
 
-      <div className="flex-1 flex flex-wrap">
-        <a
-          href=""
-          className="github mx-2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GitHub />
-        </a>
+        <div className="flex-1 flex flex-wrap">
+          <a
+              href="https://github.com/mohit810/covi-helper-website"
+              className="github mx-2"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <GitHub />
+          </a>
 
-        <a
-          className="api mx-2"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Database />
-        </a>
+          <a
+              className="api mx-2"
+              href="https://github.com/mohit810/covi-helper-backend-code"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <Database />
+          </a>
 
-        <a
-          href=""
-          className="telegram mx-2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Send />
-        </a>
+          <a
+              href=""
+              className="telegram mx-2"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <Send />
+          </a>
 
-        <a
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-          className="twitter mx-2"
-        >
-          <Twitter />
-        </a>
+          <a
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+              className="twitter mx-2"
+          >
+            <Twitter />
+          </a>
 
-        <a
-          href="mailto:"
-          className="mail mx-2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Mail />
-        </a>
-      </div>
-    </footer>
+          <a
+              href="mailto:"
+              className="mail mx-2"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            <Mail />
+          </a>
+        </div>
+      </footer>
   );
 }
 
